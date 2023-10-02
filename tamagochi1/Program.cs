f752a091a3c839bb7a26fa630e0c9d4d22505d90
HelmiKang <helmi.kangasmaa@elev.ga.ntig.se>
2023-10-02 12:01:00 +0200

    jjj

---
+++ tamagochi1/Program.cs
@@ -0,0 +1,15 @@
+﻿Player p = new();
+
+question q1 = new();
+
+q1.Text="1+4";
+q1.Alternatives = new List<string>() {"5"};
+q1.CorrectAnswer=0;
+
+question q2 = new()
+{
+    Test = "2-1";
+    Alternatives= new List<string>(){"5"};
+    CorrectAnswer=0;
+    
+}
---
+++ tamagochi1/question.cs
@@ -0,0 +1,11 @@
+﻿class question
+{
+public string Text;
+public List<string> Alternatives;
+public int CorrectAnswer;
+
+public bool Ask()
+{
+    Console.WriteLine("text");
+}
+}
