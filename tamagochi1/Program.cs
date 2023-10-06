ce2547da27994b642a2a258b909ae7edfc0016d3
HelmiKang <helmi.kangasmaa@elev.ga.ntig.se>
2023-10-06 11:05:31 +0200

    första lektionen

--- tamagochi1/Program.cs
+++ tamagochi1/Program.cs
@@ -1,1 +1,48 @@
-﻿
+﻿using System;
+
+void Clear()
+{
+Console.WriteLine("(press any key)");
+Console.ReadLine();
+Console.Clear();
+}
+
+Console.WriteLine("Välkommen till Tamagochi! Nu ska du få adoptera en gnocchi. Vad vill du döpa den? ");
+
+Gochi gnocchi = new Gochi();
+gnocchi.name = Console.ReadLine();
+Console.WriteLine($"Grattis! nu äger du {gnocchi.name} och ansvarar för dens överlevnad.");
+
+Clear();
+
+while (gnocchi.GetAlive() == true)
+{
+System.Console.WriteLine("Vad vill du göra med din gnocchi? svara med en siffra.");
+Console.WriteLine($"1: Mata {gnocchi.name}.");
+Console.WriteLine($"2: Prata med {gnocchi.name}.");
+Console.WriteLine($"3: Lär {gnocchi.name} ett nytt ord.");
+
+string Action = Console.ReadLine();
+Console.Clear();
+if (Action == "1")
+{
+gnocchi.Feed();
+}
+if (Action == "2")
+{
+gnocchi.Hi();
+}
+if (Action == "3")
+{
+gnocchi.Teach();
+}
+else
+{
+    Console.WriteLine($"Du och {gnocchi.name} stirrar på varandra i tystnad...");
+}
+gnocchi.Tick();
+}
+
+Console.WriteLine("RIP");
+
+Console.ReadLine();
---
+++ tamagochi1/Tamagochi2.cs
@@ -0,0 +1,61 @@
+﻿public class Gochi
+{
+private int hunger;
+private int boredome;
+private List<string> words = new List<string>() {"mjau"};
+private bool isAlive = true;
+private Random generator;
+public string name;
+
+// sänker Hunger
+public void Feed()
+{
+Console.WriteLine("feed");
+
+}
+
+// skriver ut ett slumpat ord från words, och anropar ReduceBoredom.
+public void Hi()
+{
+    Console.WriteLine("hi");
+}
+
+
+// lägger till ett ord i words, och anropar ReduceBoredom.
+public void Teach()
+{
+    Console.WriteLine("teach");
+}
+
+// ökar hunger och boredom, och om någon av dem kommer över 10 så blir isAlive false.
+public void Tick()
+{
+hunger++;
+boredome++;
+
+if (boredome > 10 && hunger > 10)
+{
+    isAlive = false;
+}
+}
+
+// skriver ut nuvarande hunger och boredom, och meddelar också huruvida tamagotchin lever
+public void PrintStats()
+{
+    
+}
+
+// Returnerar värdet som isAlive har
+public bool GetAlive()
+{
+return isAlive;
+}
+
+// sänker boredom
+private void ReduceBoredom()
+{
+    
+}
+
+
+}
