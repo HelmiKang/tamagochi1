589c3519243774087016a294a624153954c9dc0d
HelmiKang <helmi.kangasmaa@elev.ga.ntig.se>
2023-10-06 10:04:53 +0200

    hello

--- tamagochi1/Program.cs
+++ tamagochi1/Program.cs
@@ -1,15 +1,1 @@
-﻿Player p = new();
-
-question q1 = new();
-
-q1.Text="1+4";
-q1.Alternatives = new List<string>() {"5"};
-q1.CorrectAnswer=0;
-
-question q2 = new()
-{
-    Test = "2-1";
-    Alternatives= new List<string>(){"5"};
-    CorrectAnswer=0;
-    
-}
+﻿
